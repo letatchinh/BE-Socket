ff3524bf9c7a469cea2196a3524a4869f9ef338d
Lê Tất Chính <letatchinh123@gmail.com>
2023-12-13 13:22:20 +0700

    asd

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const express = require("express")
-const http = require("http")
-const app = express()
-const server = http.createServer(app)
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
 const io = require("socket.io")(server, {
 	cors: {
 		origin: "*",
 		methods: [ "GET", "POST" ]
 	}
-})
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Running');
+});
 
 io.on("connection", (socket) => {
-	socket.emit("me", socket.id)
+	socket.emit("me", socket.id);
 
 	socket.on("disconnect", () => {
 		socket.broadcast.emit("callEnded")
-	})
+	});
 
-	socket.on("callUser", (data) => {
-		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-	})
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
 
 	socket.on("answerCall", (data) => {
 		io.to(data.to).emit("callAccepted", data.signal)
-	})
-})
+	});
+});
 
-server.listen(5001, () => console.log("server is running on port 5001"))
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
